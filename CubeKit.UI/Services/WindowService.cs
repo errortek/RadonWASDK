e05a64c60e2c778717ca8b9164f595c7d8a1003e
ErrorTek <jakub.baranski.33@gmail.com>
2024-07-11 16:22:37 +0100

    Add project files.

---
+++ CubeKit.UI/Services/WindowService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI;
+
+namespace CubeKit.UI.Services
+{
+    public static class WindowService
+    {
+        private static Border AppTitleBar;
+        private static TextBlock AppTitle;
+
+        public static void Initialize(Border TitleBar, TextBlock Title)
+        {
+            AppTitleBar = TitleBar;
+            AppTitle = Title;
+            // TODO Windows.UI.ViewManagement.ApplicationView is no longer supported. Use Microsoft.UI.Windowing.AppWindow instead. For more details see https://docs.microsoft.com/en-us/windows/apps/windows-app-sdk/migrate-to-windows-app-sdk/guides/windowing
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            // Hide default title bar.
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            UpdateTitleBarLayout(coreTitleBar);
+
+            // Set XAML element as a draggable region.
+            App.Window.SetTitleBar(AppTitleBar);
+
+            // Register a handler for when the size of the overlaid caption control changes.
+            // For example, when the app moves to a screen with a different DPI.
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // Register a handler for when the title bar visibility changes.
+            // For example, when the title bar is invoked in full screen mode.
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+
+            //Register a handler for when the window changes focus
+            //App.Window.Activated += Current_Activated;
+        }
+
+        private static void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+
+        private static void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            // Update title bar control size as needed to account for system size changes.
+            AppTitleBar.Height = coreTitleBar.Height;
+
+            // Ensure the custom title bar does not overlap window caption controls
+            Thickness currMargin = AppTitleBar.Margin;
+            AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+        }
+
+        private static void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args) => AppTitleBar.Visibility = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+
+        // Update the TitleBar based on the inactive/active state of the app
+        private static void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            SolidColorBrush defaultForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorPrimaryBrush"];
+            SolidColorBrush inactiveForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorDisabledBrush"];
+
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            {
+                AppTitle.Foreground = inactiveForegroundBrush;
+            }
+            else
+            {
+                AppTitle.Foreground = defaultForegroundBrush;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+<img src="https://github.com/itzbluebxrry/Radon-Browser/assets/99461083/feaa49da-9efd-40a7-9aec-9754c210ae09" width="100" height="100">
+
+# Radon Browser
+
+A sleek, elegant-looking web browsing experience designed to meet the Fluent UI design principle with the fusion of Apple's Safari flavor—promising an intuitive and delightful user experience for your own personalized browsing experience. Built based on native UWP framework, Radon gives you a blazing fast browsing experience with minimal resource impact, even on lower end systems—ensuring convenience anywhere, anytime.
+
+Currently under development, I'm working hard to bring this browser to life so you can enjoy delightful browsing experience with unmatched performance and control over customizations.
+
+## Features:
+
+- Appealing detailed Ul design aligned to the Fluent design guidelines, with fusion of Apple's Safari signature looks.
+
+- Performant minimal resource impact on lower end hardware, thanks to the native UWP runtime for Windows 10/11 devices.
+
+- Handful control over customizations to your browser's looks and behavior. More customization features are coming later.
+
+- Multitasking features such as multi windowing, sidebar, AI integrations and split tabs. (Coming soon)
+
+- Powered by WebView2 engine, based on Chromium for compatibility and performance with most modern sites. WebView1 engine implementation is planned for compatibility with legacy sites.
+
+Have an improvement or feature request? Go to [GitHub Issues](https://github.com/itzbluebxrry/radon-browser/issues) and submit a new issue.
+
+## Download and installation:
+
+- Get it from [Microsoft Store](https://apps.microsoft.com/detail/Radon%20Browser%20Dev/9N38CTMDRBFF?launch=true&mode=mini) (recommended)
+
+<a href="https://apps.microsoft.com/detail/Radon%20Browser%20Dev/9N38CTMDRBFF?launch=true
+	&mode=mini">
+	<img src="https://get.microsoft.com/images/en-us%20dark.svg" width="200"/>
+</a>
+
+- Download sideloading package from GitHub releases [here](https://github.com/itzbluebxrry/radon-browser/releases/latest)
+
+## Contribute to the project
+
+I'm not opening this project for contribution yet due to the lack of time for maintenance. However, you can get in touch with me and the developer community by joining Developer Sanctuary and UWP Community Discord servers.
+
+*(p/s: I'm more active on Developer Sanctuary server though)*
+
+
+
+
